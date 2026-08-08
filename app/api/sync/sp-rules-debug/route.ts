8aa2874b22df0fdb230c52110209a8676ff82822
Nimbus111 <info@theradiologycoach.com>
2026-08-08 18:13:10 -0400

    Update debug file

--- app/api/sync/sp-rules-debug/route.ts
+++ app/api/sync/sp-rules-debug/route.ts
@@ -4,9 +4,11 @@ const AIRTABLE_BASE = process.env.AIRTABLE_BASE_ID!
 const AIRTABLE_KEY = process.env.AIRTABLE_API_KEY!
 
 export async function GET() {
-  const url = `https://api.airtable.com/v0/${AIRTABLE_BASE}/X-ray%20Service%20Provider%20State%20Rules?maxRecords=1`
-  const res = await fetch(url, { headers: { Authorization: `Bearer ${AIRTABLE_KEY}` } })
+  const res = await fetch(
+    `https://api.airtable.com/v0/meta/bases/${AIRTABLE_BASE}/tables`,
+    { headers: { Authorization: `Bearer ${AIRTABLE_KEY}` } }
+  )
   const data = await res.json()
-  const fields = data.records?.[0]?.fields || {}
-  return NextResponse.json({ fieldNames: Object.keys(fields), sample: fields })
+  const tableNames = (data.tables || []).map((t: any) => t.name)
+  return NextResponse.json({ tableNames })
 }
